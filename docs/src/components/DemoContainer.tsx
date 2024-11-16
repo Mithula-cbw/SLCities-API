8b11684ce9c641a4851240a878bb3681b01fa632
Mithula Chanthuka <mithula.cbw@gmail.com>
2024-11-16 06:08:48 +0530

    minor: delete unused import

--- docs/src/components/DemoContainer.tsx
+++ docs/src/components/DemoContainer.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import CitySearch from "./CitySearch";
-import Code from "./Code";
 
 const DemoContainer: React.FC = () => {
   const [query, setQuery] = useState('');
