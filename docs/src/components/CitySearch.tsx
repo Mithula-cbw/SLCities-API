fba7aac31638316d8c3a093124ca56fe96757c08
Mithula Chanthuka <mithula.cbw@gmail.com>
2024-11-18 17:19:08 +0530

    relocate suggestions to the searchCity component

--- docs/src/components/CitySearch.tsx
+++ docs/src/components/CitySearch.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 
+interface Suggestion {
+  id: number;
+  name: string;
+}
+
 interface CitySearchProps {
   handleInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  suggestions:Suggestion[];
   query: string;
 }
 
-const CitySearch: React.FC<CitySearchProps> = ({ handleInput, query }) => {
+const CitySearch: React.FC<CitySearchProps> = ({ handleInput, query, suggestions }) => {
   return (
-    <div className="w-full sm:w-[400px] flex flex-col sm:mx-4 my-4">
+    <div className="w-full sm:w-[400px] flex flex-col sm:mx-4 my-4 relative">
       <input
         type="text"
         id="city-search"
@@ -16,6 +22,15 @@ const CitySearch: React.FC<CitySearchProps> = ({ handleInput, query }) => {
         placeholder="Type a city name..."
         className="p-2 border-future-600 dark:bg-gray-900 border rounded-md focus:outline-none focus:ring-2 focus:ring-future-600"
       />
+        {suggestions.length > 0 && 
+          <ul className="suggestions z-40 bg-black absolute top-[100%]">
+          {suggestions.map((suggestion) => (
+            <li key={suggestion.id}>{suggestion.name}</li>
+          ))}
+        </ul>
+        
+        
+        }
     </div>
   );
 };
--- docs/src/components/DemoContainer.tsx
+++ docs/src/components/DemoContainer.tsx
@@ -55,18 +55,7 @@ const DemoContainer: React.FC = () => {
     <div className="w-full text-left p-2 shadow-md mt-10 font-poppins rounded-sm border">
       <h2 className="text-xs sm:text-sm m-2">Give Our API a Spin and See it in Action</h2>
       <div className="w-full flex flex-col justify-start items-start p-0">
-        <CitySearch handleInput={handleInputChange} query={query} />
-        {isLoading && <p>Loading...</p>}
-        {suggestions.length > 0 ? (
-          <ul className="suggestions fixed z-40 bg-black">
-          {suggestions.map((suggestion) => (
-            <li key={suggestion.id}>{suggestion.name}</li>
-          ))}
-        </ul>
-        
-        ) : (
-          !isLoading && query && <p>No matches found for "{query}"</p>
-        )}
+        <CitySearch handleInput={handleInputChange} query={query} suggestions={suggestions}/>         
         <DemoOptions queryTemplate={fullQuery} response={response} />
       </div>
     </div>
