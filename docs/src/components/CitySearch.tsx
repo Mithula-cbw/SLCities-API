ffe348b4fe12fadf853ff7112791b06e2bb55e11
Mithula Chanthuka <mithula.cbw@gmail.com>
2024-11-19 07:41:55 +0530

    Improve the suggestions by making it only appear while search is focused

--- docs/src/components/CitySearch.tsx
+++ docs/src/components/CitySearch.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Suggestion {
   id: number;
@@ -7,34 +7,40 @@ interface Suggestion {
 
 interface CitySearchProps {
   handleInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  suggestions:Suggestion[];
+  suggestions: Suggestion[];
   query: string;
 }
 
 const CitySearch: React.FC<CitySearchProps> = ({ handleInput, query, suggestions }) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
   return (
     <div className="w-full sm:w-[400px] flex flex-col sm:mx-4 my-4 relative">
       <input
         type="text"
         id="city-search"
         value={query}
-        onChange={handleInput} 
+        onChange={handleInput}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         placeholder="Type a city name..."
         className="p-2 border-future-600 dark:bg-gray-900 border rounded-md focus:outline-none focus:ring-2 focus:ring-future-600"
       />
-        {suggestions.length > 0 ? (
-          <ul className="suggestions z-40 bg-black absolute top-[100%] rounded-md mt-[1px] w-full">
+      {isFocused && suggestions.length > 0 && (
+        <ul className="suggestions z-40 bg-black absolute top-[100%] rounded-md mt-[1px] w-full">
           {suggestions.map((suggestion) => (
-            <li className="p-1 bg-gray-500 px-3 drop-shadow-sm" key={suggestion.id}>{suggestion.name}</li>
+            <li
+              className="p-1 bg-gray-500 px-3 drop-shadow-sm"
+              key={suggestion.id}
+            >
+              {suggestion.name}
+            </li>
           ))}
-          
         </ul>
-         ) : (
-          // !isLoading && query && <p>No matches found for "{query}"</p>
-          <></>
-        )}
-        
-
+      )}
     </div>
   );
 };
